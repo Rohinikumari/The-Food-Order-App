d412f424328dcf558e4641fa0d9d047a69d9bc20
Rohini Kumari <rohinikumari2010@gmail.com>
2023-02-09 21:05:37 +0530

    UI changes for Cart CURD operation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Fragment } from "react";
+import Cart from "./components/Cart/Cart";
+
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
@@ -6,6 +8,7 @@ import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <Fragment>
+     <Cart/>
       <Header />
       <main>
         <Meals />
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,11 @@
-import classes from "./MealItem.module.css";
+
+
+import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
-    const price = `$${props.price.toFixed(2)}`
+  const price = `$${props.price.toFixed(2)}`;
+
   return (
     <li className={classes.meal}>
       <div>
@@ -9,8 +13,11 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <di></di>
+      <div>
+        <MealItemForm id={props.id} />
+      </div>
     </li>
   );
 };
+
 export default MealItem;
